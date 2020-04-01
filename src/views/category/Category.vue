b60ddddab16d37ee7ac3ee9fbbf709f547a1959a
Michael1094 <rocknwoods@163.com>
2020-04-01 09:17:27 +0800

    初始化项目

---
+++ src/views/category/Category.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class=''>分类</div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data(){
+    return {}
+  },
+  components: {},
+  created(){},
+  mounted(){},
+  methods: {}
+}
+</script>
+<style scoped>
+</style>
